b8efb57a4eb242ce1c5a4ee37e869f28863a27d8
COCOBALL <70007131+eunchae2000@users.noreply.github.com>
2022-09-30 11:43:20 +0900

    자바 배열 응용 실습

---
+++ java03/src/배열응용/기본형복사.java
@@ -0,0 +1,18 @@
+package 배열응용;
+
+public class 기본형복사 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int 수학1 = 100;
+		int 수학2 = 수학1;
+		
+		System.out.println("수학1: " + 수학1);
+		System.out.println("수학2: " + 수학2);
+		수학2 = 50;
+		System.out.println("수학1: " + 수학1);
+		System.out.println("수학2: " + 수학2);
+
+	}
+
+}
